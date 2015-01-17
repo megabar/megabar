e5780f840149b9906f69c2d27dc244636d465e2a
Bagus <bagus1@github.com>
2015-01-17 10:06:11 -0700

    inherit from megabar::applicationcontroller

--- lib/generators/mega_bar/mega_bar_models/templates/generic_controller.rb
+++ lib/generators/mega_bar/mega_bar_models/templates/generic_controller.rb
@@ -1,9 +1,8 @@
 <% if the_module_name %>
 module <%=the_module_name%>
-  <% end %>class <%= the_controller_name %> < ApplicationController
+  <% end %>class <%= the_controller_name %> < MegaBar::ApplicationController
     include MegaBar::MegaBarConcern
     before_action ->{ myinit <%= model_id %> },  only: [:index, :show, :edit, :new]
-
 
     private
       def _params
