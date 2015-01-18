9dd06e4920840722357e89ac2079dd7910a47f59
Bagus <bagus1@github.com>
2015-01-18 08:13:42 -0700

    remove _params from controller generator

--- lib/generators/mega_bar/mega_bar_models/templates/generic_controller.rb
+++ lib/generators/mega_bar/mega_bar_models/templates/generic_controller.rb
@@ -1,15 +1,6 @@
 <% if the_module_name %>
 module <%=the_module_name%>
   <% end %>class <%= the_controller_name %> < MegaBar::ApplicationController
-    include MegaBar::MegaBarConcern
-    private
-      def _params
-        permits = []
-        controller_name.classify.constantize.attribute_names.each do |att|
-          permits << att unless ['id', 'created_at', 'updated_at'].include?(att)
-        end
-        params.require(controller_name.singularize).permit(permits)
-      end
-
+    include MegaBar::MegaBarConcern    
 <% if the_module_name %>  end<% end %>
 end
