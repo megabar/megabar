7187f82a337ae51795e891583f5aa496c1a44fe0
Bagus <bagus1@github.com>
2015-02-08 17:58:49 -0700

    wrap up model_display_format work

--- db/migrate/20150208182051_create_mega_bar_tmp_model_display_formats.rb
+++ db/migrate/20150208182051_create_mega_bar_tmp_model_display_formats.rb
@@ -3,16 +3,16 @@ class CreateMegaBarTmpModelDisplayFormats < ActiveRecord::Migration
     create_table :mega_bar_tmp_model_display_formats do |t|
       t.datetime :created_at
       t.datetime :updated_at
-      string :name
-      string :app_wrapper
-      string :app_wrapper_end
-      string :field_header_wrapper
-      string :field_header_wrapper_end
-      string :record_wrapper
-      string :record_wrapper_end
-      string :field_wrapper
-      string :field_wrapper_end
-      string :separate_header_row
+      t.string :name
+      t.string :app_wrapper
+      t.string :app_wrapper_end
+      t.string :field_header_wrapper
+      t.string :field_header_wrapper_end
+      t.string :record_wrapper
+      t.string :record_wrapper_end
+      t.string :field_wrapper
+      t.string :field_wrapper_end
+      t.string :separate_header_row
     end
   end
 end
