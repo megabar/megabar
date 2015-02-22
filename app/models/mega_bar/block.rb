732cb290840196bf0e5055062e693f041ac01774
Bagus <bagus1@github.com>
2015-02-22 15:14:15 -0700

    fix block magic

--- app/models/mega_bar/block.rb
+++ app/models/mega_bar/block.rb
@@ -11,11 +11,12 @@ module MegaBar
       if self.model_id
         model_name = Model.find(self.model_id).name
         actions = []
-        actions << {:format=>2, :action=>'new', :header=>'Create ' + model_name.singularize}  if (!ModelDisplay.by_model(self.model_id).by_action('new').present? && @new_model_display == 'y')
-        actions << {:format=>2, :action=>'edit', :header=>'Edit ' + model_name.singularize} if (!ModelDisplay.by_model(self.model_id).by_action('edit').present? && @edit_model_display == 'y')
-        actions << {:format=>1, :action=>'index', :header=>model_name.pluralize} if (!ModelDisplay.by_model(self.model_id).by_action('index').present? && @index_model_display == 'y')
-        actions << {:format=>2, :action=>'show', :header=>model_name.singularize} if (!ModelDisplay.by_model(self.model_id).by_action('show').present? && @show_model_display == 'y')
+        actions << {:format=>2, :action=>'new', :header=>'Create ' + model_name.singularize}  if (!ModelDisplay.by_block(self.id).by_action('new').present? && @new_model_display == 'y')
+        actions << {:format=>2, :action=>'edit', :header=>'Edit ' + model_name.singularize} if (!ModelDisplay.by_block(self.id).by_action('edit').present? && @edit_model_display == 'y')
+        actions << {:format=>1, :action=>'index', :header=>model_name.pluralize} if (!ModelDisplay.by_block(self.id).by_action('index').present? && @index_model_display == 'y')
+        actions << {:format=>2, :action=>'show', :header=>model_name.singularize} if (!ModelDisplay.by_block(self.id).by_action('show').present? && @show_model_display == 'y')
         log_arr = []
+        byebug
         actions.each do | action |
           md = ModelDisplay.create(:block_id=>self.id, model_id: self.model_id, :format=>action[:format], :action=>action[:action], :header=>action[:header])
         end
