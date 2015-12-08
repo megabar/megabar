d83886c093701290f843dff66e7565a43050f736
Bagus <bagus.haig@placewise.com>
2015-12-08 10:17:23 -0700

    index and all

--- app/controllers/mega_bar/pages_controller.rb
+++ app/controllers/mega_bar/pages_controller.rb
@@ -2,15 +2,18 @@ module MegaBar
   class PagesController < MegaBar::ApplicationController
     include MegaBar::MegaBarConcern
 
-    def indexx
+    def index
       admin_pages = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
       @mega_instance ||= Page.where(['id not in (?)', admin_pages ]).order(column_sorting)
-      super.index
+      super
     end
     def all
       @mega_instance = Page.all.order(column_sorting)
       index
     end
-
+    def edit
+      session[:return_to] = request.referer
+      super
+    end
   end
 end
