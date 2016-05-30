c565becc13aea99d6e16f8e0ea6509b747fb5a3f
Bagus <bagus.haig@placewise.com>
2016-05-29 19:03:05 -0600

    fix page path from created model bug

--- app/models/mega_bar/model.rb
+++ app/models/mega_bar/model.rb
@@ -31,7 +31,7 @@ module MegaBar
     def make_page_for_model
       if !self.make_page.nil? && !self.make_page.empty?
         mod = self.modyule.nil? || self.modyule.empty?  ? '' : self.modyule.underscore + '/'
-        path = '/' + mod + self.tablename
+        path = '/' + mod.dasherize + self.classname.underscore.dasherize
         # path = self.make_page == 'default_model_path' ? path : self.make_page
         Page.create(name: self.name + ' Model Page', path: path, make_layout_and_block: 'y', base_name: self.name, model_id: self.id )
       end
