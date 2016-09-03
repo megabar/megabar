3d3db6c1528cc08bf16f2699a10e74adcf85be9e
bagus1 <bagus@comcast.net>
2016-09-03 12:09:14 -0600

    prepopulate model_display_id on mdc new form

--- app/controllers/mega_bar/model_display_collections_controller.rb
+++ app/controllers/mega_bar/model_display_collections_controller.rb
@@ -1,5 +1,10 @@
 module MegaBar
   class ModelDisplayCollectionsController < MegaBar::ApplicationController
-    include MegaBar::MegaBarConcern    
+    include MegaBar::MegaBarConcern  
+
+    def new
+      @model_display_id = params["model_display_id"] if params["model_display_id"]
+      super
+    end   
   end
 end 
