a06b350975a45fe5aa4b7c734b6ba12d824ed059
bagus1 <bagus@comcast.net>
2016-09-15 11:34:18 -0600

    no foreign_key needed here

--- app/models/mega_bar/site_join.rb
+++ app/models/mega_bar/site_join.rb
@@ -1,7 +1,7 @@
 module MegaBar 
   class SiteJoin < ActiveRecord::Base
     belongs_to :siteable, polymorphic: true #, optional: true
-    belongs_to :site, foreign_key: :siteable_id
+    belongs_to :site
     validates_presence_of :siteable_type, allow_blank: false
   end
 end 
