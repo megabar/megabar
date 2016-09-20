3d42f66e4292b576c525555451c9950e571f47b1
bagus1 <bagus@comcast.net>
2016-09-20 11:04:58 -0600

    layout templating stuffs

--- db/migrate/20160919180435_add_css_framework_to_mega_bar_layouts.rb
+++
@@ -1,5 +0,0 @@
-class AddCssFrameworkToMegaBarLayouts < ActiveRecord::Migration
-  def change
-    add_column :mega_bar_layouts, :css_framework, :string
-  end
-end
--- db/migrate/20160919180438_add_css_framework_to_mega_bar_tmp_layouts.rb
+++
@@ -1,5 +0,0 @@
-class AddCssFrameworkToMegaBarTmpLayouts < ActiveRecord::Migration
-  def change
-    add_column :mega_bar_tmp_layouts, :css_framework, :string
-  end
-end
