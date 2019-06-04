1d2a38dc875641ebe3dded49998672fffe6369e5
John Haig <jhaig.contractor@ofiglobal.com>
2019-06-04 10:45:46 -0600

    fixes template-section and theme/site issues

--- app/models/mega_bar/site.rb
+++ app/models/mega_bar/site.rb
@@ -1,11 +1,11 @@
-module MegaBar 
+module MegaBar
   class Site < ActiveRecord::Base
     belongs_to :portfolio
     belongs_to :theme
-    belongs_to :siteable, polymorphic: true
+    # belongs_to :siteable, polymorphic: true
     has_many :site_joins
     has_many :layouts, through: :site_joins, source: :siteable, source_type: 'Layout'
-    has_many :blocks, through: :site_joins, source: :siteable, source_type: 'Block'    
+    has_many :blocks, through: :site_joins, source: :siteable, source_type: 'Block'
     validates_uniqueness_of :code_name
     validates_presence_of :code_name, allow_blank: false
     validates_presence_of :portfolio_id, allow_blank: false
@@ -13,4 +13,4 @@ module MegaBar
     validates_presence_of :name, allow_blank: false
 
   end
-end 
+end
--- app/models/mega_bar/theme.rb
+++ app/models/mega_bar/theme.rb
@@ -1,9 +1,9 @@
-module MegaBar 
+module MegaBar
   class Theme < ActiveRecord::Base
     has_many :sites
     has_many :portfolios
 
-    belongs_to :themeable, polymorphic: true
+    # belongs_to :themeable, polymorphic: true
     has_many :theme_joins
     has_many :layouts, through: :theme_joins, source: :themeable, source_type: 'Layout'
     has_many :blocks, through: :theme_joins, source: :themeable, source_type: 'Block'
@@ -11,4 +11,4 @@ module MegaBar
     validates_presence_of :code_name, allow_blank: false
     validates_presence_of :name, allow_blank: false
   end
-end 
+end
