9569a3bdecc5ceed0ec49d8b5028959ad57534a6
John Haig <jhaig.contractor@ofiglobal.com>
2019-06-24 10:08:02 -0600

    uses booleans for checkboxes

--- app/helpers/mega_bar/models_helper.rb
+++ app/helpers/mega_bar/models_helper.rb
@@ -1,12 +1,13 @@
 module MegaBar
   module ModelsHelper
     def pre_render
-      if params[:action] == 'edit' 
+      if params[:action] == 'edit'
         # these are for viratual attributes
-        @index_model_display = ModelDisplay.by_model(@model.id).by_action('index').present? ? 'y':'' 
-        @show_model_display = ModelDisplay.by_model(@model.id).by_action('show').present? ? 'y':'' 
-        @new_model_display = ModelDisplay.by_model(@model.id).by_action('new').present? ? 'y':'' 
-        @edit_model_display = ModelDisplay.by_model(@model.id).by_action('edit').present? ? 'y':'' 
+        byebug
+        @index_model_display = ModelDisplay.by_model(@model.id).by_action('index').present?
+        @show_model_display = ModelDisplay.by_model(@model.id).by_action('show').present?
+        @new_model_display = ModelDisplay.by_model(@model.id).by_action('new').present?
+        @edit_model_display = ModelDisplay.by_model(@model.id).by_action('edit').present?
       end
     end
   end
--- app/models/mega_bar/block.rb
+++ app/models/mega_bar/block.rb
@@ -43,10 +43,10 @@ module MegaBar
       if (!self.model_id.nil? && !self.model_id.to_s.empty? && Integer(self.model_id) > 0)
         model_name = Model.find(self.model_id).name
         actions = []
-        actions << {:format=>2, :action=>'new', :header=>'Create ' + model_name.humanize.singularize, collection_or_member: 'member'}  if (!ModelDisplay.by_block(self.id).by_action('new').present? && @new_model_display == 'y')
-        actions << {:format=>2, :action=>'edit', :header=>'Edit ' + model_name.humanize.singularize,  collection_or_member: 'member'} if (!ModelDisplay.by_block(self.id).by_action('edit').present? && @edit_model_display == 'y')
-        actions << {:format=>1, :action=>'index', :header=>model_name.humanize.pluralize,  collection_or_member: 'collection'} if (!ModelDisplay.by_block(self.id).by_action('index').present? && @index_model_display == 'y')
-        actions << {:format=>2, :action=>'show', :header=>model_name.humanize.singularize,  collection_or_member: 'member'} if (!ModelDisplay.by_block(self.id).by_action('show').present? && @show_model_display == 'y')
+        actions << { :format =>2, :action=>'new', :header=>'Create ' + model_name.humanize.singularize, collection_or_member: 'member'}  if (!ModelDisplay.by_block(self.id).by_action('new').present? && @new_model_display)
+        actions << {:format=>2, :action=>'edit', :header=>'Edit ' + model_name.humanize.singularize,  collection_or_member: 'member'} if (!ModelDisplay.by_block(self.id).by_action('edit').present? && @edit_model_display)
+        actions << {:format=>1, :action=>'index', :header=>model_name.humanize.pluralize,  collection_or_member: 'collection'} if (!ModelDisplay.by_block(self.id).by_action('index').present? && @index_model_display)
+        actions << {:format=>2, :action=>'show', :header=>model_name.humanize.singularize,  collection_or_member: 'member'} if (!ModelDisplay.by_block(self.id).by_action('show').present? && @show_model_display)
         actions.each do | action |
           md = ModelDisplay.create(:block_id=>self.id, model_id: self.model_id, :format=>action[:format], :action=>action[:action], :header=>action[:header], collection_or_member: action[:collection_or_member])
         end
