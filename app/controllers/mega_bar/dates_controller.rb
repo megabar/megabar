3cdec5dbee307b756f6bd15cfef0721d8a303a4c
Bagus1 <bagus@comcast.net>
2025-06-03 14:07:04 -0600

    date thing

--- app/controllers/mega_bar/dates_controller.rb
+++ app/controllers/mega_bar/dates_controller.rb
@@ -4,6 +4,8 @@ module MegaBar
 
 class DatesController < MegaBar::ApplicationController
     include MegaBar::MegaBarConcern    
+    skip_before_action :check_authorization, only: [:index]
+
 end
 
 
