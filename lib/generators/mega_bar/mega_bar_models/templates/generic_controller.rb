f3fb546a095523160a68d7708f817e74f22b572b
Bagus1 <bagus@comcast.net>
2025-09-12 10:11:41 -0600

    generates a better controller

--- lib/generators/mega_bar/mega_bar_models/templates/generic_controller.rb
+++ lib/generators/mega_bar/mega_bar_models/templates/generic_controller.rb
@@ -4,7 +4,6 @@ module <%=m %>
 
 class <%= the_controller_name %> < MegaBar::ApplicationController
     include MegaBar::MegaBarConcern
-    load_and_authorize_resource if defined?(Cccux::ApplicationControllerConcern)
 end
 
 <% the_module_array.each do | m | %>
